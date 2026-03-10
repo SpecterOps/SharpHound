ecd26b33da86ffc6b8ef9a9597ce4905f91845d0
Lucas Falslev <lfalslev@specterops.io>
2026-03-10 16:52:47 -0600

    pipeline test

--- src/Runtime/ObjectProcessors.cs
+++ src/Runtime/ObjectProcessors.cs
@@ -52,7 +52,8 @@ namespace Sharphound.Runtime {
             _domainTrustProcessor = new DomainTrustProcessor(context.LDAPUtils);
             _computerAvailability = new ComputerAvailability(context.PortScanTimeout,
                 skipPortScan: context.Flags.SkipPortScan, skipPasswordCheck: context.Flags.SkipPasswordAgeCheck);
-            _certAbuseProcessor = new CertAbuseProcessor(context.LDAPUtils, new RegistryAccessor(), new SAMServerAccessor());
+            //TODO: temp change to test github build pipeline
+            _certAbuseProcessor = new CertAbuseProcessor(context.LDAPUtils, new RegistryAccessor(), new SAMServerAccessor(), "");
             _dcRegistryProcessor = new DCRegistryProcessor(context.LDAPUtils);
             _computerSessionProcessor = new ComputerSessionProcessor(context.LDAPUtils,
                 doLocalAdminSessionEnum: context.Flags.DoLocalAdminSessionEnum,
